f1fd7eb6dd100a0d284b6c4c0ecc86edf1e800c6
Carson Sievert <cpsievert1@gmail.com>
2016-01-22 18:21:06 -0600

    deprecate repo

--- published/r_urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-import api_docs.views
-
-urlpatterns = patterns(
-   '',
-    url("(?P<user_guide_chapter>user-guide)/$", api_docs.views.user_guide_template)
-)
